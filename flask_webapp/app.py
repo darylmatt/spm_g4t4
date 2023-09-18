81cce7e03f38fd61aaabaf5d7fc9324aa839a0fc
Joon Xiang Yong <jxyong.2021@scis.smu.edu.sg>
2023-09-19 01:38:00 +0800

    Updated template

--- flask_webapp/app.py
+++ flask_webapp/app.py
@@ -2,8 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/design_reference')
+def design_reference():
+    dynamic_content = "This content is coming from Flask!"
+    #Keep design reference untouched
+    return render_template("design_reference.html")
+
 @app.route('/')
 def index():
+    #This will be our base, customised template that pages will follow
     dynamic_content = "This content is coming from Flask!"
     return render_template("index.html")
 
@@ -16,8 +23,6 @@ def listings():
 def applied_roles():
     dynamic_content = "This content is coming from Flask!"
     return render_template("applied_roles.html")
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
