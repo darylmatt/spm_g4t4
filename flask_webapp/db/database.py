31bf29ffc7f3646d8cf7b0720f63e0044c05b0c8
hangsinghui <shhang.2021@scis.smu.edu.sg>
2023-09-23 00:36:57 +0800

    remove sensitive db credentials

--- flask_webapp/db/database.py
+++ flask_webapp/db/database.py
@@ -5,7 +5,6 @@ def create_db():
     app = Flask(__name__)
 
     # Load database configurations from config.py or elsewhere
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://spm_g4t4:password@42.61.167.85:3306/spm_g4t4'
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
