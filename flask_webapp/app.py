5347b7efcd50a4fb51d93e7401ff4f366af2ca7b
ciara1940 <ciaracheah.2021@scis.smu.edu.sg>
2023-09-25 20:24:15 +0800

    created applied_roles function

--- flask_webapp/app.py
+++ flask_webapp/app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS
 from db_config.db import db
+from datetime import datetime
 from sqlalchemy.exc import SQLAlchemyError
 from flask_sqlalchemy import SQLAlchemy
 from db_config.models import *
+from sqlalchemy import text
+
 
 app = Flask(__name__)
 
@@ -56,10 +59,49 @@ def listings():
     dynamic_content = "This content is coming from Flask!"
     return render_template("listings.html")
 
-@app.route('/applied_roles_staff')
-def applied_roles():
-    dynamic_content = "This content is coming from Flask!"
-    return render_template("applied_roles.html")
+@app.route('/applied_roles_staff/<int:listing_id>', methods=["POST"])
+def applied_roles(listing_id):
+    try:
+        staff_id = 19  # REPLACE with the actual staff_id
+        status = "Pending"
+        applied_date = datetime.now()
+
+        # Check if the listing exists
+        role_listing = Role_Listing.query.filter_by(listing_id=listing_id).first()
+        if not role_listing:
+            return jsonify({"error": "Role listing not found"}), 404
+
+        # Check if the listing is closed (past the application deadline)
+        current_datetime = datetime.now()
+        if role_listing.date_open > current_datetime or role_listing.date_close < current_datetime:
+            return jsonify({"error": "Role listing is closed or not yet open for applications"}), 400
+
+        # Check if the staff member has already applied to this listing
+        existing_application = Application.query.filter_by(listing_id=listing_id, staff_id=staff_id).first()
+        if existing_application:
+            return jsonify({"error": "You have already applied to this role"}), 400
+
+        # insert application details
+        insert_sql = text("""
+            INSERT INTO application (listing_id, staff_id, status, applied_date)
+            VALUES (:listing_id, :staff_id, :status, :applied_date)
+        """)
+
+        params = {
+            "listing_id": listing_id,
+            "staff_id": staff_id,
+            "status": status,
+            "applied_date": applied_date,
+        }
+        
+        db.session.execute(insert_sql, params)
+        db.session.commit()
+
+        return jsonify({"message": "Application submitted successfully"}), 201
+
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 500
 
 @app.route('/role_creation')
 def role_creation():
