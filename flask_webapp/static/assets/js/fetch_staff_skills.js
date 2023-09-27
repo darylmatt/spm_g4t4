0737620628b0e5a1916a7a914b478901c0e4daf1
hangsinghui <shhang.2021@scis.smu.edu.sg>
2023-09-27 14:22:59 +0800

    Update fetch_staff_skills.js

--- flask_webapp/static/assets/js/fetch_staff_skills.js
+++ flask_webapp/static/assets/js/fetch_staff_skills.js
@@ -1,21 +1,16 @@
 // Wait for the DOM to be fully loaded
 document.addEventListener("DOMContentLoaded", function () {
-  // Get a reference to the "Skills" tab button by its ID
   const skillsTabButton = document.getElementById("skillsTabButton");
-  console.log("Yes pressed");
+  console.log("Loaded");
 
-  // Add a click event listener to the "Skills" tab button
-  // skillsTabButton.addEventListener("click", function () {
-  // Make an AJAX request to the Flask route that returns skills data
   fetch("/skills")
     .then((response) => response.json())
     .then((data) => {
       // Access and render the skills data in the skills card
       const skillsCard = document.getElementById("skillsCard");
-      const skillsList = data.data; // Access the "data" field from the JSON response
+      const skillsList = data.data;
       console.log(skillsList);
 
-      // Loop through the skills and create HTML elements for each skill
       skillsList.forEach((skill) => {
         const skillCard = document.createElement("div");
         skillCard.classList.add("card");
