93c63831488ef6bb0e28dd8bcd95151d04217775
Angie <louiseangiee@gmail.com>
2023-10-30 14:45:51 +0800

    changes for merge conflict

--- flask_webapp/test_config.py
+++ flask_webapp/test_config.py
@@ -1,5 +1,6 @@
 class TestConfig:
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://g4t4:password@spm-g4t4-sbrp.cybxkypjkirc.ap-southeast-2.rds.amazonaws.com:3306/sbrp_test'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY='Our_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    # SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://g4t4:password@spm-g4t4-sbrp.cybxkypjkirc.ap-southeast-2.rds.amazonaws.com:3306/sbrp_test'
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SECRET_KEY='Our_secret_key'
